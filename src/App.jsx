0c38da3f9d00237897b87e912a31b7e7d1d579d7
Maximus <tomiwadairo@gmail.com>
2024-04-23 15:13:31 +0100

    tweaked the background a little

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Nav  from './Components/Nav';
 import Heading from './Components/Heading';
 import Balance from './Components/Balance';
@@ -13,7 +12,7 @@ function App() {
     <GlobalProvider >
       <div>
       <h5 className="text-gray-700 text-3xl pt-80 pb-10 font-bold ">
-        Working fine, as of April 16, 2024.
+        Working fine, as of April 19, 2024.
           </h5>
           <Nav/>
           <Heading/>
